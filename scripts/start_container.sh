439a475f4bef3d41e6ad47513d9c9cc729692aab
Sampath24 <lcskumar@gmail.com>
2024-10-24 19:57:14 -0500

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull sampathlc/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:8000 sampathlc/simple-python-flask-app
+docker run -d -p 5000:5000 sampathlc/simple-python-flask-app
